254ce3904984676d65cfa38e51e6afe444affdcf
Adrian Canilao <canilaoadrian28@gmail.com>
2026-05-10 00:19:48 +0800

    update

--- attendance-system/src/pages/EditEmployee.jsx
+++ attendance-system/src/pages/EditEmployee.jsx
@@ -22,9 +22,34 @@ export default function EditEmployee() {
   }, []);
 
   const fetchEmployees = async () => {
-    const { data } = await supabase.from("employee_profiles").select("*");
+  // GET CURRENT USER
+  const { data: authData } =
+    await supabase.auth.getUser();
+
+  const currentUserId = authData.user.id;
+
+  // GET MANAGER BRANCH
+  const { data: profile } = await supabase
+    .from("employee_profiles")
+    .select("branch_id")
+    .eq("id", currentUserId)
+    .single();
+
+  if (!profile) return;
+
+  // FETCH ONLY SAME BRANCH EMPLOYEES
+  const { data, error } = await supabase
+    .from("employee_profiles")
+    .select("*")
+    .eq("branch_id", profile.branch_id)
+    .order("full_name", {
+      ascending: true,
+    });
+
+  if (!error) {
     setEmployees(data || []);
-  };
+  }
+};
 
   const filtered = employees.filter((emp) =>
     emp.full_name.toLowerCase().includes(search.toLowerCase())
