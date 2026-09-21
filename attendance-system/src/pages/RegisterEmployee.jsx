3ca931cfe40241f8d0b92f3881ab3a823a612f71
AdrianCanilao <canilaoadrian28@gmail.com>
2026-09-21 11:10:17 +0800

    fix deployment environment fallbacks

--- attendance-system/src/pages/RegisterEmployee.jsx
+++ attendance-system/src/pages/RegisterEmployee.jsx
@@ -4,8 +4,8 @@ import { supabase } from "../supabaseClient";
 import ManagerLayout from "../layouts/ManagerLayout";
 import { logAudit } from "../utils/auditLogger";
 
-const API_URL = (import.meta.env.VITE_API_URL || API_URL + "").replace(/\/$/, "");
-const INSIGHTFACE_URL = (import.meta.env.VITE_INSIGHTFACE_URL || INSIGHTFACE_URL + "").replace(/\/$/, "");
+const API_URL = (import.meta.env.VITE_API_URL || "http://127.0.0.1:8000").replace(/\/$/, "");
+const INSIGHTFACE_URL = (import.meta.env.VITE_INSIGHTFACE_URL || "http://127.0.0.1:8002").replace(/\/$/, "");
 
 export default function RegisterEmployee() {
   const webcamRef = useRef(null);
