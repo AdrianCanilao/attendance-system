d8d4dfdb825e06b462e5e4875f4e1472238fbfd8
AdrianCanilao <canilaoadrian28@gmail.com>
2026-09-26 02:09:44 +0800

    Fix kiosk audit logger import

--- attendance-system/src/pages/Kiosk.jsx
+++ attendance-system/src/pages/Kiosk.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import Webcam from "react-webcam";
 import { KIOSK_CODE } from "../kioskConfig";
+import { logAudit } from "../utils/auditlogger";
 
 const INSIGHTFACE_URL = (import.meta.env.VITE_INSIGHTFACE_URL || "http://127.0.0.1:8002").replace(/\/$/, "");
 const BACKEND_URL = (import.meta.env.VITE_API_URL || "http://127.0.0.1:8000").replace(/\/$/, "");
